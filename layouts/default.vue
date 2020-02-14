62a515c6a832d54a137ca4b4af4d6b43234ef9a4
Jing Li <jing_li1998@hotmail.com>
2020-02-14 23:54:42 +1100

    layout finished including background, image and column sizing

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,117 +1,47 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
+  <v-app style="background-color: #8EE144">
+    <!-- <v-app-bar fixed app>
       <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
+    </v-app-bar>-->
     <v-content>
-      <v-container>
-        <nuxt />
+      <v-container style="height: 100%" fluid>
+        <v-row style="height: 100%" class="d-flex flex-column justify-center">
+          <v-col style="min-height:75%" sm="8" offset-sm="2" class="pa-0">
+            <v-card
+              style="border-radius:30px"
+              raised
+              class="main-form d-flex justify-center pa-0"
+              height="100%"
+            >
+              <v-row class="pa-0">
+                <v-col sm="4" class="pa-0">
+                  <v-img
+                    style="border-radius:30px 0 0 30px  "
+                    src="/background3.jpg"
+                    class="pa-0"
+                    height="100%"
+                    width="100%"
+                  ></v-img>
+                </v-col>
+                <v-col sm="8" class style="border-radius:0 30px 30px 0">
+                  <nuxt />
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
+      items: [],
+      title: "Microfinance Calculator"
+    };
   }
-}
+};
 </script>
