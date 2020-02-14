5d60d8b7ecb4793f2a207d0090627d160c0c32a8
Jing Li <jing_li1998@hotmail.com>
2020-02-15 00:38:35 +1100

    added step 1 form

---
+++ components/ProductType.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-form>
+    <p
+      class="headline"
+    >Please describe the features of the loan. Later we will ask for further information on each feature you have selected</p>
+    <v-row justify="center">
+      <v-col cols="11">
+        <v-select
+          v-for="item in items"
+          :items="item.options"
+          :label="item.text"
+          :rules="[v => !!v || 'Item is required']"
+          :key="item.id"
+          required
+        ></v-select>
+      </v-col>
+    </v-row>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: {
+        fees: {
+          text: "Click Fees if fees apply",
+          options: ["Fees", "No Fees"]
+        },
+        collateral: {
+          text: "Click Collateral if cash collateral is required",
+          options: ["Collateral", "No Collateral"]
+        },
+        grace: {
+          text: "Click Grace Period if there is any grace period",
+          options: ["Grace Period", "No Grace Period"]
+        },
+        gross: {
+          text:
+            "Click Gross Witholding Tax if Maha will be required to pay GWT",
+          options: ["No Gross Witholding Tax", "Gross WItholding Tax"]
+        },
+        repayment: {
+          text:
+            "Click Regular if repayments follow a regular rule. Click Irregular if you need to enter repayments manually",
+          options: ["Regular", "Irregular"]
+        },
+        interest: {
+          text:
+            "Click Varies Over Time if the interest rate changes over the life of the loan",
+          options: ["Varies Over Time", "Constant Over Time"]
+        }
+      }
+    };
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,35 @@
 <template>
   <v-row style="height:100%" align="center" justify="center">
     <v-col cols="10" align="center">
-      <p class="text-center display-3">Welcome to the Microfinance Calculator</p>
-      <v-btn text class="headline" color="primary">Click here to begin</v-btn>
+      <div v-show="current=='landingPage'">
+        <p class="text-center display-3">Welcome to the Microfinance Calculator</p>
+        <v-btn
+          text
+          class="headline"
+          @click="current='productType'"
+          color="primary"
+        >Click here to begin</v-btn>
+      </div>
+      <!-- <landing-page></landing-page> -->
+      <product-type v-show="current=='productType'"></product-type>
     </v-col>
   </v-row>
 </template>
 
 <script>
-export default {};
+// import LandingPage from "../components/LandingPage.vue";
+import ProductType from "../components/ProductType.vue";
+export default {
+  components: {
+    // LandingPage,
+    ProductType
+  },
+  data() {
+    return {
+      current: "landingPage"
+    };
+  }
+};
 </script>
 
 <style scoped>
