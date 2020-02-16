c94468405e8ba03bb111804f7c1f24a92f7a1b10
Jing Li <jing_li1998@hotmail.com>
2020-02-16 22:46:12 +1100

    add forms for collateral and grace period

--- pages/calculator/collateral.vue
+++ pages/calculator/collateral.vue
@@ -1,3 +1,36 @@
 <template>
-  <div class="headline">Collateral</div>
+  <div>
+    <div class="headline">Collateral</div>
+    <v-text-field label="Fixed Collateral" type="number" step="0.01" prefix="$" />
+    <v-text-field
+      label="Percent Collateral"
+      type="number"
+      step="0.01"
+      suffix="as % of loan amount"
+    />
+    <v-row>
+      <v-col cols="6">
+        <v-text-field label="Interest on collateral" type="number" step="0.01" suffix="%" />
+      </v-col>
+      <v-col cols="6">
+        <v-select
+          :items="items"
+          label="Expressed"
+          :rules="[v => !!v || 'Item is required']"
+          required
+        />
+      </v-col>
+    </v-row>
+    <v-text-field label="Interest accrues every..." type="number" suffix="month(s)" />
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: ["Yearly", "Monthly"]
+    };
+  }
+};
+</script>
--- pages/calculator/grace-period.vue
+++ pages/calculator/grace-period.vue
@@ -1,3 +1,31 @@
 <template>
-  <div class="headline">Grace Period</div>
+  <div>
+    <div class="headline">Grace Period</div>
+    <v-text-field label="Grace period for first..." type="number" suffix="month(s)" />
+
+    <v-row>
+      <v-col cols="6">
+        <v-select
+          :items="items"
+          label="Applies to interest?"
+          :rules="[v => !!v || 'Item is required']"
+          required
+        />
+      </v-col>
+      <v-col cols="6">
+        <v-select
+          :items="items"
+          label="Applies to principal?"
+          :rules="[v => !!v || 'Item is required']"
+          required
+        />
+      </v-col>
+    </v-row>
+  </div>
 </template>
+
+<script>
+export default {
+  items: ["Yes", "No"]
+};
+</script>
