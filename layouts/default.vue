42c1c8281aae82e213e213254d95819ef8990950
Jing Li <jing_li1998@hotmail.com>
2020-02-17 16:54:57 +1100

    update background and images

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,30 +2,15 @@
   <v-app style="background-image: url('/main.jpg')">
     <div class="overlay" />
     <v-content>
-      <v-container style="height: 100%" fluid>
-        <v-row style="height: 100%" class="d-flex flex-column align-center justify-center">
-          <v-col style="min-height:90%" sm="10" class="pa-0">
-            <v-card
-              style="border-radius:10px"
-              raised
-              class="main-form d-flex justify-center pa-0"
-              height="100%"
-            >
-              <!-- <v-col sm="4" class="pa-0">
-                  <v-img
-                    style="border-radius:30px 0 0 30px  "
-                    src="/background3.jpg"
-                    class="pa-0"
-                    height="100%"
-                    width="100%"
-                  ></v-img>
-              </v-col>-->
-              <!-- <v-col sm="10" class style="border-radius:0 30px 30px 0"> -->
-              <nuxt />
-              <!-- </v-col> -->
-            </v-card>
-          </v-col>
-        </v-row>
+      <v-container style="height: 100%" class="pa-10" fluid>
+        <v-card
+          style="border-radius:10px"
+          raised
+          class="main-form d-flex justify-center pa-0"
+          height="100%"
+        >
+          <nuxt />
+        </v-card>
       </v-container>
     </v-content>
   </v-app>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <v-row style="height:100%" align="center" justify="center">
-    <v-col cols="12" align="center">
+  <v-row style="height:100%" align="center" justify="center" class="pl-3">
+    <v-col sm="4" class="tree pa-0" />
+    <v-col cols="8" align="center">
       <p class="text-center display-3">Welcome to the Microfinance Calculator</p>
       <nuxt-link tag="div" to="/calculator/product-type">
         <v-btn text class="headline" color="primary">Click here to begin</v-btn>
@@ -8,3 +9,4 @@
     </v-col>
   </v-row>
 </template>
+   
